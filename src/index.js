a3a66e2ce994b54e00dd1b81e1229d5c089b69f1
Nishant Kumar <nishantdinayak11@gmail.com>
2022-03-03 12:25:28 +0530

    change

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+const elem = <App />;
 
-
+ReactDOM.render(element, document.getElementById('root'));
